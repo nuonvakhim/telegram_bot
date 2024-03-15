426613571cca75d26604c2e09bbe1be9a4e2f6ed
vakhimnuon <vakhimnuon@gmail.com>
2024-03-16 01:32:57 +0700

    random image

--- src/main/java/com/vakhim/telegram_bot/TelegramBot.java
+++ src/main/java/com/vakhim/telegram_bot/TelegramBot.java
@@ -1,6 +1,4 @@
 package com.vakhim.telegram_bot;
-
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -10,15 +8,20 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
+import java.util.Random;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
-//
+    //
+    @Override
+    public String getBotToken() {
+        return "";
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
         String text = update.getMessage().getText();
         Long chatId = update.getMessage().getChatId();
-//
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chatId);
         sendMessage.setText(text);
@@ -26,30 +29,53 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         String command = update.getMessage().getText();
 
-        if (command.equals("/run")){
-            String mes = "Kom Plex Leb Tnam ";
+        if (command.equals("/run")) {
+            String mes = "Mnak nis srolanh tea oun";
             SendMessage sendMessage1 = new SendMessage();
             sendMessage1.setChatId(update.getMessage().getChatId().toString());
             sendMessage1.setText(mes);
             try {
                 execute(sendMessage1);
 
-            }catch (TelegramApiException e){
+            } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
-        }
-//        InputFile file = new InputFile();
-//        file.setMedia(new File("D:\\telegrambot\\FB_IMG_1710386820722.jpg"));
-//
-//
-//        SendPhoto sendPhoto = new SendPhoto();
-//        sendPhoto.setChatId(chatId);
-//        sendPhoto.setPhoto(file);
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            new RuntimeException(e);
-        }
+        } else if (command.equals("/chat")) {
+            String mes = "Chat tov ke mun tov";
+            SendMessage sendMessage1 = new SendMessage();
+            sendMessage1.setChatId(update.getMessage().getChatId().toString());
+            sendMessage1.setText(mes);
+            try {
+                execute(sendMessage1);
+
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (command.equals("/send")) {
+            // Array of your image paths
+            String[] images = {"D:\\user01\\telegram\\crushbnang.jpg", "D:\\user01\\telegram\\FB_IMG_1710386827284.jpg", "D:\\user01\\telegram\\image3.jpg"};
+
+            // Create a Random object
+            Random rand = new Random();
+
+            // Generate a random index in the range of your array
+            int randomIndex = rand.nextInt(images.length);
+
+            // Select a random image path from your array
+            String randomImagePath = images[randomIndex];
+
+            InputFile file = new InputFile();
+            file.setMedia(new File(randomImagePath));
+
+            SendPhoto sendPhoto = new SendPhoto();
+            sendPhoto.setChatId(chatId);
+            sendPhoto.setPhoto(file);
+            try {
+                execute(sendPhoto);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+            }
         System.out.println(text);
     }
 
